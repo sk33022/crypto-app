41e086dc2a01790bc6dadd71766ea7163634928f
sk33022 <53243995+sk33022@users.noreply.github.com>
2022-06-07 08:12:02 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const filteredCoins = coins.filter(
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
         </li>
       </ul>
       <form className="d-flex" role="search">
